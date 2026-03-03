efb3a0d1f36ce7eb2b69311c2f2049771df1d134
Aydin <159723594+AydinTechnology@users.noreply.github.com>
2026-03-03 22:13:39 +0100

    Update setup_openmpi.sh

--- setup_openmpi.sh
+++ setup_openmpi.sh
@@ -1,18 +1,21 @@
 #for quick setting up of master/worker nodes on Debian live usb (requires root)
 
-
 #the user provides this script with a hostname which will be used to set up this node
 read -r given_hostname
-
 
 #install necessary Open MPI runtime
 sudo apt update
 sudo apt install -y openssh-server openmpi-bin libopenmpi-dev
 
-#change hostname so that nodes can recognise eachother
-sudo hostnamectl set-hostname master
-
-
 #set up SSH (necessary for Open MPI to communicate to other nodes)
 sudo systemctl enable ssh
 sudo systemctl start ssh
+
+#change hostname so that nodes can recognise eachother by name instead of hardcoded local IP adresses (no, i'm not willing to disable DHCP on my home network!)
+sudo hostnamectl set-hostname $given_hostname
+
+print("Log out and in again to have the hostname change take effect")
+print("Todo: edit /etc/hosts and add in the format: ip hostname ;on each system")
+print("Also create a hostfile for openmpi on the master node in the format: hostname slots=x ;in which x is amount of CPU cores available")
+print("Remember to run: ssh-keygen -t ed25519 ;on the master node")
+print("And on the master node: ssh-copy-id user@hostname ;to each node other than the master")
