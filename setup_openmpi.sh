13f0de008fe30cf272bf5c622a1e597bb19191ff
Aydin <159723594+AydinTechnology@users.noreply.github.com>
2026-03-03 09:30:46 +0100

    Update setup_openmpi.sh

--- setup_openmpi.sh
+++ setup_openmpi.sh
@@ -4,7 +4,7 @@
 sudo apt update
 sudo apt install -y openssh-server openmpi-bin libopenmpi-dev
 
-#allow unprotected SSH (dangerous cybersecurity-wise, but there are probably no hackers on our network)
+#allow unprotected SSH so no messing with passwords or keys or fingerprints or whatever (very insecure security-wise, but this makes setup quite a bit easier, and since we're airgapped anyways, who cares?)
 sudo sed -i 's/PermitEmptyPasswords no/PermitEmptyPasswords yes/' /etc/ssh/sshd_config
 sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
 sudo sed -i 's/PermitRootLogin no/PermitRootLogin yes/' /etc/ssh/sshd_config
